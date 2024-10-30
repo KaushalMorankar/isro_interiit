7a4200499c89414fb2efd5a50f6f72de25dd58fc
breakdown-voltage <akshathkumarch1405@gmail.com>
2024-10-31 01:26:40 +0530

    Add background noise extraction

---
+++ project/codes/data_pre_processing/background.py
@@ -0,0 +1,103 @@
+import numpy as np
+from astropy.io import fits
+import os
+
+class io():
+    def __init__(self, filename:str):
+        self.name = filename
+    
+    def readFile(self):
+        return fits.open(self.name)
+    
+    
+    def getData(self):
+        '''
+        Returns:
+            A 2-D Array containing channels and counts in a FITS file
+        '''
+        with self.readFile() as hdu:
+            data = hdu[1].data
+            channels = data['CHANNEL']
+            counts = data['COUNTS']
+            return np.column_stack((channels, counts)).astype(np.float64)
+    
+    def backgroundNoise(self):
+        '''
+        Returns: 
+            True if the satellite is in the darkside of the moon
+            otherwise False
+        '''
+        with self.readFile() as hdu:
+            return hdu[1].header['SOLARANG']>90
+        
+
+def background_process(input_folder:str, output_folder:str):
+    """
+    Processes X-ray fluorescence (XRF) data files in the specified folder, calculates 
+    background noise if detected, and saves the processed data as FITS files.
+
+    Parameters:
+    -----------
+    input_folder : str
+        Path to the folder containing the input data files
+    output_folder : str
+        Path to the folder where processed FITS files will be saved
+
+    Workflow:
+    ---------
+    1. Reads files from `input_folder` in reverse order(To prioritize background data before processing XRF data).
+    2. Checks each file for background noise. If background noise is detected:
+       - Accumulates the counts to calculate an average background signal.
+    3. For non-background files:
+       - If background noise has been detected in previous files, it averages 
+         the accumulated background.
+       - Writes the XRF signal and background noise to a FITS file with the original header.
+       - Saves the output FITS file in `output_folder` with a name format `filteredData_<filename>`.
+    4. The saved FITS files are structured as follows:
+        - Header of raw data in the header of Primary HDU
+        - XRF_Signal contains channels and counts
+        - background contains background noise calculated
+
+    Returns:
+    --------
+    None
+    """
+    files = [file for file in os.listdir(input_folder)][::-1]
+
+    background_detection = False
+    background = np.zeros((2048, 1))
+    iters = 0
+    
+    for file in files:
+        path = f"{input_folder}/{file}"
+        data = io(path)
+        hdr = data.readFile()
+        is_background = data.backgroundNoise()
+
+        if is_background:
+            iters += 1
+            if not background_detection:
+                background_detection = True
+                background = np.zeros((2048, 1))
+            counts = data.getData()[:, 1].reshape((2048, 1))
+            background += counts
+            print(background.shape)
+        
+        else:
+            if background_detection:
+                background_detection = False
+                # Doesnt process the XRF data without getting background noise
+                if iters == 0:
+                    continue
+                background /= iters
+                iters = 0
+            
+            header = hdr[1].header
+            # XRF_Signal contains both background noise and XRF data
+            XRF_signal = data.getData()
+            hdu1 = fits.ImageHDU(XRF_signal, name='XRFSignal')
+            hdu2 = fits.ImageHDU(background, name='BackgroundNoise')
+
+            hdul = fits.HDUList([fits.PrimaryHDU(header=header), hdu1, hdu2])
+            output_path = f"{output_folder}/filteredData_{file}"
+            hdul.writeto(output_path, overwrite=True)
